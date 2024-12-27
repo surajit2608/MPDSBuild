e9fd29e1f1f4f293295c559ef2f9b42c5827491e
Surajit Pramanik <surajit@wisely.co>
2024-12-27 21:02:28 +0530

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import { v4 as uuidv4 } from 'uuid'
 
 const SocialLink = ({ slug, name }) => (
   <a className="smi" rel="noopener" target="_blank" href={slug}>
-    <img src={`assets/svgs/${name.toLowerCase()}.svg`} alt={name} width="42" height="42" />
+    <img src={`/img/${name.toLowerCase()}.svg`} alt={name} width="42" height="42" />
   </a>
 )
 
