102221e513a0080cb3c16ce2c55d366971589a9d
Surajit Pramanik <surajit@wisely.co>
2024-12-28 14:56:34 +0530

    html

--- src/html.js
+++ src/html.js
@@ -2,51 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import NoScript from './components/NoScript'
 export default function HTML(props) {
-
-  const faviconLetter = (bgcolor, fontcolor, letter) => {
-    var canvas = document.createElement('canvas'),
-      ctx = canvas.getContext('2d'),
-      size = 32,
-      roundRect = function (ctx, x, y, width, height, radius, fill, stroke) {
-        if (typeof stroke == "undefined") {
-          stroke = true;
-        }
-        if (typeof radius === "undefined") {
-          radius = 6;
-        }
-        ctx.beginPath();
-        ctx.moveTo(x + radius, y);
-        ctx.lineTo(x + width - radius, y);
-        ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
-        ctx.lineTo(x + width, y + height - radius);
-        ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
-        ctx.lineTo(x + radius, y + height);
-        ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
-        ctx.lineTo(x, y + radius);
-        ctx.quadraticCurveTo(x, y, x + radius, y);
-        ctx.closePath();
-        if (stroke) {
-          ctx.stroke();
-        }
-        if (fill) {
-          ctx.fill();
-        }
-      };
-
-    canvas.width = canvas.height = size;
-    ctx.save();
-    ctx.fillStyle = bgcolor;
-    roundRect(ctx, 0, 0, size, size, 8, true, false);
-    ctx.font = "16px Calibri";
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-    ctx.fillStyle = fontcolor;
-    ctx.fillText(letter.toUpperCase(), size / 2, (size / 2) + 2);
-    ctx.restore();
-
-    document.querySelector("link[rel='shortcut icon']").setAttribute('href', canvas.toDataURL("img/png"));
-  }
-
   return (
     <html {...props.htmlAttributes}>
       <head>
